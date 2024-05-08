6b8dd5601ae850a4389fd6ddd5c5ac4114891eca
Danny4life <osiki.segun@gmail.com>
2024-05-08 08:00:34 +0100

    add files

--- src/main/java/week_3/class_activity_3A/Bike.java
+++ src/main/java/week_3/class_activity_3A/Bike.java
@@ -1,4 +1,9 @@
 package week_3.class_activity_3A;
 
 public class Bike extends Vehicle{
+     //do this to show while overriding a method is important for specific implementation
+//    void run(){
+//        System.out.println("Bike is running safely");
+//    }
+
 }
