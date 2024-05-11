b60381ef25bf2296e7f4bdabe9eea96004848eb9
Danny4life <osiki.segun@gmail.com>
2024-05-11 09:40:35 +0100

    edit file

--- src/main/java/week_3/class_activity_12A/JavaExceptionEx.java
+++ src/main/java/week_3/class_activity_12A/JavaExceptionEx.java
@@ -4,17 +4,18 @@ public class JavaExceptionEx {
     public static void main(String[] args) {
         // this is the first example to give
 
-//        int num = 500 / 0; // exception  may occur
+//        int num = 50 / 0; // exception  may occur
 //        System.out.println("run other parts of the code");
 
 
         // this is the second example
 
 //        try{
-//            int num = 500/0;
+//            int num = 50/0;
 //        } catch (Exception e) {
 //            System.out.println(e);
 //        }
 //        System.out.println("run other parts of the code");
+
     }
 }
