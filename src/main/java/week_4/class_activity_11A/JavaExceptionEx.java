0ff5c55aa517a552e601498ae7199cfa27d9388d
Danny4life <osiki.segun@gmail.com>
2024-05-13 19:01:52 +0100

    add files

---
+++ src/main/java/week_4/class_activity_11A/JavaExceptionEx.java
@@ -0,0 +1,24 @@
+package week_4.class_activity_11A;
+
+public class JavaExceptionEx {
+    public static void main(String[] args) {
+
+         //this is the first example to give
+
+        int num = 500 / 0; // exception  may occur
+        System.out.println("run other parts of the code");
+
+
+        // this is the second example
+
+//        try{
+//            int num = 500/0;
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
+//        System.out.println("run other parts of the code");
+
+
+    }
+
+}
