dde799e953f4435ae661315cba19e9b16b1c5888
Danny4life <osiki.segun@gmail.com>
2024-05-21 07:47:22 +0100

    add file

---
+++ src/main/java/week_5/class_exercise_3A/model/Book.java
@@ -0,0 +1,42 @@
+package week_5.class_exercise_3A.model;
+
+public class Book {
+    private int id;
+    private String bookTitle;
+    private String bookAuthor;
+
+    private int qty;
+
+    public Book(int id, String bookTitle, String bookAuthor, int qty) {
+        this.id = id;
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.qty = qty;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", bookTitle='" + bookTitle + '\'' +
+                ", bookAuthor='" + bookAuthor + '\'' +
+                ", qty=" + qty +
+                '}';
+    }
+}
